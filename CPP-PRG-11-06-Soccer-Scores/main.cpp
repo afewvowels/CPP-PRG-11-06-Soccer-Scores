5079717b940746a193f40599c3d416c0a193f94e
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-07 19:29:46 -0500

    Prompt written.

--- CPP-PRG-11-06-Soccer-Scores/main.cpp
+++ CPP-PRG-11-06-Soccer-Scores/main.cpp
@@ -5,11 +5,27 @@
 //  Created by Keith Smith on 11/7/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  Write a program that stores the following data about a soccer player in a structure:
+//
+//      Player's Name
+//      Player's Number
+//      Points Scored by Player
+//
+//  The program should keep an array of 12 of these structures. Each element is for a different
+//  player on a team. When the program runs, it should ask the user to enter the data
+//  for each player. It should then show a table that lists each player's number, name, and
+//  points scored. The program should also calculate and display the total points earned by
+//  the team. The number and name of the player who has earned the most points should
+//  also be displayed.
+//
+//  Input Validation: Do not accept negative values for players' numbers or points scored.
+//
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
     return 0;
 }
